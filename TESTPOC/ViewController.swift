89240a214dcf5c4fbe336d262ecede861cb1d584
Mahammad Sahnawaj Biswas <mahammad.sahnawajbiswas@gmail.com>
2020-06-28 19:38:43 +0530

    Initial Commit

---
+++ TESTPOC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TESTPOC
+//
+//  Created by Biswas, Mahammad Sahnawaj (Cognizant) on 28/06/20.
+//  Copyright © 2020 Biswas, Mahammad Sahnawaj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
